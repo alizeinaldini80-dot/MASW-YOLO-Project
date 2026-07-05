e9bd123ef6bbf0a433d83d16627cd9aff7564f61
alizeinaldini80-dot <alizeinaldini80@gmail.com>
2026-07-05 16:49:30 +0330

    Update __init__.py

--- src/models/modules/__init__.py
+++ src/models/modules/__init__.py
@@ -1,5 +1,11 @@
 # src/models/modules/__init__.py
-from .msca import MSCA
-from .afpn import ASFF2, ASFF3, BasicBlock
+from .modules.msca import MSCA
+from .modules.afpn import ASFF2, ASFF3, BasicBlock
+import ultralytics.nn.tasks as tasks
+
+tasks.MSCA = MSCA
+tasks.ASFF2 = ASFF2
+tasks.ASFF3 = ASFF3
+tasks.BasicBlock = BasicBlock
 
 __all__ = ["MSCA", "ASFF2", "ASFF3", "BasicBlock"]
