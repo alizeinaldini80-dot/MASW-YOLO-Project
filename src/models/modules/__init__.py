0c4f603e20ce6a907f0fdb02b0ce5fbabcf9eed3
alizeinaldini80-dot <alizeinaldini80@gmail.com>
2026-07-15 11:52:25 +0330

    Update __init__.py

--- src/models/modules/__init__.py
+++ src/models/modules/__init__.py
@@ -1,5 +1,5 @@
 # src/models/modules/__init__.py
 from .msca import MSCA
-from .afpn import ASFF2, ASFF3, BasicBlock
+from .afpn import ASFF2, ASFF3, ASFF4, AFPN, BasicBlock, Index, Conv
 
-__all__ = ["MSCA", "ASFF2", "ASFF3", "BasicBlock"]
+__all__ = ["MSCA", "ASFF2", "ASFF3", "ASFF4", "AFPN", "BasicBlock", "Index", "Conv"]
